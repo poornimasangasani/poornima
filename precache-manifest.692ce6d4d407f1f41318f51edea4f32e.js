2a4844aff84e06dd3ed7d8783266fd060c6484ba
poornima <poornimasangasani@gmail.com>
2020-07-04 12:39:10 +0530

    Updates

---
+++ precache-manifest.692ce6d4d407f1f41318f51edea4f32e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9072d086e8ee7e42275e28dd9af3b28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "972a35217e67cc053e6b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0ce5f183b5b8072ffaff",
+    "url": "/static/js/2.b609c125.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b609c125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "972a35217e67cc053e6b",
+    "url": "/static/js/main.2380b9f2.chunk.js"
+  },
+  {
+    "revision": "417eec8e7f319d2c4617",
+    "url": "/static/js/runtime-main.08d55f92.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/poornima/precache-manifest.956f3c0bded3288daf98ccda8d09af33.js"
+  "/precache-manifest.692ce6d4d407f1f41318f51edea4f32e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/poornima/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
