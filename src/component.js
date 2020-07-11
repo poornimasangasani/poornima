b05a3ff896aeec7f3d90f8f8fec29fec551499cb
poornima <poornimasangasani@gmail.com>
2020-07-11 23:03:06 +0530

    add some files

--- src/component.js
+++ src/component.js
@@ -1,65 +1,28 @@
 import React, { Component } from "react";
-import { Layout, Menu } from "antd";
-import {
-  MenuUnfoldOutlined,
-  MenuFoldOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
-} from "@ant-design/icons";
+import { Layout } from "antd";
 import "antd/dist/antd.css";
 
-const { Header, Sider, Content } = Layout;
+// import "./App.css";
+// import Image from "../src/assets/bground.png";
+// import {
+//   MenuUnfoldOutlined,
+//   MenuFoldOutlined,
+//   UserOutlined,
+//   VideoCameraOutlined,
+//   ProfileOutlined,
+//   UploadOutlined,
+//   HomeOutlined,
+// } from "@ant-design/icons";
+
+const { Header, Content, Footer } = Layout;
 
 class Page extends Component {
-  state = {
-    collapsed: false,
-  };
-
-  toggle = () => {
-    this.setState({
-      collapsed: !this.state.collapsed,
-    });
-  };
-
   render() {
     return (
-      <Layout>
-        <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
-          <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-            <Menu.Item key="1" icon={<UserOutlined />}>
-              nav 1
-            </Menu.Item>
-            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-              nav 2
-            </Menu.Item>
-            <Menu.Item key="3" icon={<UploadOutlined />}>
-              nav 3
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout className="site-layout">
-          <Header className="site-layout-background" style={{ padding: 0 }}>
-            {React.createElement(
-              this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
-              {
-                className: "trigger",
-                onClick: this.toggle,
-              }
-            )}
-          </Header>
-          <Content
-            className="site-layout-background"
-            style={{
-              margin: "24px 16px",
-              padding: 24,
-              minHeight: 280,
-            }}
-          >
-            Content
-          </Content>
-        </Layout>
+      <Layout style={{ height: "100vh" }}>
+        <Header style={{ backgroundColor: "grey" }}>Header</Header>
+        <Content>Content</Content>
+        <Footer>Footer</Footer>
       </Layout>
     );
   }
