e4340488b38f86e6f02afaeed275985ac22b456e
poornima <poornimasangasani@gmail.com>
2020-07-12 17:23:47 +0530

    add some changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,6 @@ class Home extends React.Component {
   render() {
     return (
       <div className="bgGround">
-        {/* <img src={image} alt="bg" className="homeImage" /> */}
         <h1>Poornima Here ...</h1>
       </div>
     );
